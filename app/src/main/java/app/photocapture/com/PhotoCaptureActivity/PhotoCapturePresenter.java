e06ba6a83fc9dfeeb7396508f004c9d79d8f1ce4
mkhan9047 <mkhan9047@gmail.com>
2019-10-10 08:36:21 +0600

    design done first phase

--- app/src/main/java/app/photocapture/com/PhotoCaptureActivity/PhotoCapturePresenter.java
+++ app/src/main/java/app/photocapture/com/PhotoCaptureActivity/PhotoCapturePresenter.java
@@ -1,4 +1,12 @@
 package app.photocapture.com.PhotoCaptureActivity;
 
 public class PhotoCapturePresenter {
+    private void createFolder() {
+    }
+
+    private void saveVideo() {
+    }
+
+    private void saveImage() {
+    }
 }

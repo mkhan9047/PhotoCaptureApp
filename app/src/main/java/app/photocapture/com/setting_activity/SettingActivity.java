60ac43056c57e74a9be0269fb74965f6ab48b261
mkhan9047 <mkhan9047@gmail.com>
2019-10-23 07:37:17 +0600

    image quality design done

--- app/src/main/java/app/photocapture/com/setting_activity/SettingActivity.java
+++ app/src/main/java/app/photocapture/com/setting_activity/SettingActivity.java
@@ -2,12 +2,17 @@ package app.photocapture.com.setting_activity;
 
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import app.photocapture.com.R;
 import app.photocapture.com.util.Constants;
@@ -18,6 +23,11 @@ public class SettingActivity extends AppCompatActivity implements
 
     RadioGroup inputTypeRadioGroup;
     Switch aSwitchPreviewImage;
+    CheckBox customImageQualityChecker;
+    Spinner imageQualitySpinner;
+    ConstraintLayout layoutImageQualityPercentage;
+    SeekBar seekBarQuality;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +36,29 @@ public class SettingActivity extends AppCompatActivity implements
         initViews();
         setUpToolbar();
         setListener();
+        customImageQualityChecker.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    layoutImageQualityPercentage.setVisibility(View.VISIBLE);
+                    imageQualitySpinner.setEnabled(false);
+                    SharedPrefUtils.INSTANCE.write(Constants.PreferenceKeys.IMAGE_QUALITY_TYPE_IS_CUSTOM,
+                            true);
+                } else {
+                    SharedPrefUtils.INSTANCE.write(Constants.PreferenceKeys.IMAGE_QUALITY_TYPE_IS_CUSTOM,
+                            false);
+                    layoutImageQualityPercentage.setVisibility(View.GONE);
+                    imageQualitySpinner.setEnabled(true);
+                }
+            }
+        });
     }
 
     private void setUpToolbar() {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle(getResources().getString(R.string.txt_setting));
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         }
     }
 
@@ -40,9 +67,14 @@ public class SettingActivity extends AppCompatActivity implements
         aSwitchPreviewImage.setOnCheckedChangeListener(this);
     }
 
+
     private void initViews() {
+        customImageQualityChecker = findViewById(R.id.checkbox_custom);
+        imageQualitySpinner = findViewById(R.id.spinner_quality);
+        layoutImageQualityPercentage = findViewById(R.id.layout_custom_image_quality);
         inputTypeRadioGroup = findViewById(R.id.radio_group);
         aSwitchPreviewImage = findViewById(R.id.switch_image_preview);
+        seekBarQuality = findViewById(R.id.seek_bar_image_quality);
     }
 
     @Override
@@ -79,8 +111,8 @@ public class SettingActivity extends AppCompatActivity implements
     private void showSavedPreviewOption() {
         aSwitchPreviewImage.setChecked(
                 SharedPrefUtils.INSTANCE.readPreViewImageStatus(
-                Constants.PreferenceKeys.IS_PREVIEW_IMAGE_ON
-        ));
+                        Constants.PreferenceKeys.IS_PREVIEW_IMAGE_ON
+                ));
     }
 
     @Override
